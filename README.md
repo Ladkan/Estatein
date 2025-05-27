d93deca7e7e612b19a323cf80ae44687e939f27e
Ladkan <37418569+Ladkan@users.noreply.github.com>
2025-05-27 18:23:11 +0200

    done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Estatein
 
+[Estatein - Real Estate Website Template](https://www.figma.com/community/file/1314076616839640516)
---
+++ js/main.js
@@ -0,0 +1,4 @@
+function menuToggle(){
+    document.querySelector(".burger").classList.toggle("open");
+    document.querySelector(".mobile_menu_wrapper").classList.toggle("open");
+}
